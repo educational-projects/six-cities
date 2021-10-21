8676b2279fdd1267c833ccef5b9938a8b7a5d2be
ваше имя <dimarik-tixonov@yandex.ru>
2021-10-21 21:00:31 +0300

    переиспользует компонент картлиста и карточки для offers

--- project/src/components/card-list/card-list.tsx
+++ project/src/components/card-list/card-list.tsx
@@ -4,14 +4,23 @@ import ItemCard from '../card/card';
 type CardListProps = {
   cards: Offers
   onActiveCard?: (id: number | null) => void;
+  typeList?: string;
+  typeCard?: string
 }
 
-function CardList({cards, onActiveCard}: CardListProps): JSX.Element {
+const defaultClass = 'cities__places-list places__list tabs__content';
+const nearClass = 'near-places__list places__list';
+
+function CardList({cards, onActiveCard, typeList='default', typeCard}: CardListProps): JSX.Element {
+  const defaultList = typeList === 'default';
+
   return (
-    <div className="cities__places-list places__list tabs__content">
-      {cards.map((card) => (<ItemCard card={card} key={card.id} onActiveCard={onActiveCard}/>))}
+    <div className={defaultList ? defaultClass : nearClass}>
+      {cards.map((card) => (<ItemCard card={card} key={card.id} typeCard={typeCard} onActiveCard={onActiveCard}/>))}
     </div>
   );
 }
 
 export default CardList;
+
+
