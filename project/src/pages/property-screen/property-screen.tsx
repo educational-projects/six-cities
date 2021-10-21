c2edd3b4a2cccad3382d378c3fef0b9cd57d732d
ваше имя <dimarik-tixonov@yandex.ru>
2021-10-21 19:52:59 +0300

    создаст компонент комментария

--- project/src/pages/property-screen/property-screen.tsx
+++ project/src/pages/property-screen/property-screen.tsx
@@ -1,5 +1,5 @@
 import { Redirect, useParams } from 'react-router';
-import CommentForm from '../../components/comment-form/comment-form';
+import CommentList from '../../components/comment-list/comment-list';
 import Header from '../../components/header/header';
 import HostList from '../../components/host-list/host-list';
 import ImageList from '../../components/image-list/image-list';
@@ -69,34 +69,7 @@ function Property({cards}: PropertyProps): JSX.Element {
               </div>
               <OptionList options={currentCard?.goods}/>
               <HostList host={currentCard?.host} description={currentCard?.description}/>
-              <section className="property__reviews reviews">
-                <h2 className="reviews__title">Reviews &middot; <span className="reviews__amount">1</span></h2>
-                <ul className="reviews__list">
-                  <li className="reviews__item">
-                    <div className="reviews__user user">
-                      <div className="reviews__avatar-wrapper user__avatar-wrapper">
-                        <img className="reviews__avatar user__avatar" src="img/avatar-max.jpg" width="54" height="54" alt="Reviews avatar"/>
-                      </div>
-                      <span className="reviews__user-name">
-                      Max
-                      </span>
-                    </div>
-                    <div className="reviews__info">
-                      <div className="reviews__rating rating">
-                        <div className="reviews__stars rating__stars">
-                          <span style={{width: '80%'}}></span>
-                          <span className="visually-hidden">Rating</span>
-                        </div>
-                      </div>
-                      <p className="reviews__text">
-                      A quiet cozy and picturesque that hides behind a a river by the unique lightness of Amsterdam. The building is green and from 18th century.
-                      </p>
-                      <time className="reviews__time" dateTime="2019-04-24">April 2019</time>
-                    </div>
-                  </li>
-                </ul>
-                <CommentForm/>
-              </section>
+              <CommentList/>
             </div>
           </div>
           <section className="property__map map"></section>
