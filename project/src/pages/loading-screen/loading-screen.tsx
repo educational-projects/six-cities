e33f34c9809f3ec690bdd2607ca97f2cf6b3ba29
ваше имя <dimarik-tixonov@yandex.ru>
2021-11-03 22:26:30 +0300

    подключит библиотеку react-spinners

--- project/src/pages/loading-screen/loading-screen.tsx
+++ project/src/pages/loading-screen/loading-screen.tsx
@@ -1,10 +1,11 @@
+import { CircleLoader } from 'react-spinners';
 import styles from './loading-screen.module.css';
 
 function Loader(): JSX.Element {
   return (
     <div className={styles.container}>
       <div className={styles.imageWrapper}>
-        <img src="../../img/loading.gif" alt="loading" />
+        <CircleLoader color='white'/>
       </div>
     </div>
   );
